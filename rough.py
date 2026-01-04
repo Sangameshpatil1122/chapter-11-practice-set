2d2830c5a35c52ddc5266d7cbcd02bb444696416
Sangameshpatil1122 <sangameshgoudap@gmail.com>
2026-01-04 18:44:11 +0530

    added something to rough

--- rough.py
+++ rough.py
@@ -1,1 +1,17 @@
-print("Hello, World!")
+pos=neg=zero=0
+print("Enter numbers one by one. Type 'a' to finish.")
+while True:
+    num_str= input("Enter a number or (a to stop): ")
+    if num_str=='a':
+        break
+    num=int(num_str)
+    if num>0:
+        pos+=1
+    elif num<0:
+        neg+=1
+    else:
+        zero+=1
+
+print("Positive numbers:", pos)
+print("Negative numbers:", neg) 
+print("Zeros:", zero)
